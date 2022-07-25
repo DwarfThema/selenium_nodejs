5a73d02cabb12088c4d41e52cec138e17dbf09af
Junho Park <dwarfthema@gmail.com>
2022-07-25 20:49:15 +0900

    naver

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const selenium = require("selenium-webdriver");
+const { By, selenium } = require("selenium-webdriver");
 
 const driver = new selenium.Builder()
   .forBrowser(selenium.Browser.CHROME)
@@ -7,17 +7,39 @@ const driver = new selenium.Builder()
 (async function mySelenium() {
   try {
     await driver.get("https://polyhaven.com/models");
-    const region = await driver
-      .findElement(
-        selenium.By.xpath(`//div[@class='GridItem_text__HzSp8']/h3/text()`)
-      )
-      .getText();
-    console.log(region.h3.text);
+    const region = await driver.findElement(
+      By.xpath(`//div[@class="GridItem_text__HzSp8"]/h3`)
+    );
+    console.log(region);
   } catch (error) {
     console.log(error);
   } finally {
     setTimeout(() => {
       driver.quit();
-    }, 3);
+    }, 5);
   }
 })();
+
+/* const selenium = require("selenium-webdriver");
+
+const driver = new selenium.Builder()
+  .forBrowser(selenium.Browser.CHROME)
+  .build();
+
+(async function mySelenium() {
+  try {
+    await driver.get("https://www.naver.com/");
+    const region = await driver.findElement(
+      web.By.xpath(`//span[@class="_1syGnXOL _3VkgqBXB"]/span`).getAttribute(
+        "innerHTML"
+      )
+    );
+    console.log(region);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    setTimeout(() => {
+      driver.quit();
+    }, 5);
+  }
+})(); */
