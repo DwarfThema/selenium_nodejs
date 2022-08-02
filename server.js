e5742283aeb6cf32b7bb1bb467b7cb07ff379c47
Junho Park <dwarfthema@gmail.com>
2022-08-02 23:53:21 +0900

    scroll fin

--- server.js
+++ server.js
@@ -8,34 +8,45 @@ async function mySelenium() {
   try {
     await driver.get("https://polyhaven.com/models");
 
-    await driver
-      .findElements(selenium.By.xpath(`//a[@class="GridItem_gridItem__0cuEz"]`))
-      .then(async function (productAnchor) {
-        for (var i = 1; i < productAnchor.length; i++) {
-          console.log(`숫자 : `, i);
-          productAnchor[i].getAttribute(`href`).then(function (href) {
-            console.log(`자료 주소 : `, href);
-          });
+    driver
+      .executeScript(
+        `window.scrollTo({top: document.body.scrollHeight, behavior:"smooth"})`
+      )
+      .then(
+        setTimeout(async () => {
+          await driver
+            .findElements(
+              selenium.By.xpath(`//a[@class="GridItem_gridItem__0cuEz"]`)
+            )
+            .then(async function (productAnchor) {
+              for (var i = 1; i < productAnchor.length; i++) {
+                console.log(`숫자 : `, i);
+                productAnchor[i].getAttribute(`href`).then(function (href) {
+                  console.log(`자료 주소 : `, href);
+                });
 
-          const div = await productAnchor[i].findElement(
-            selenium.By.className("GridItem_thumb__M8icc")
-          );
+                const div = await productAnchor[i].findElement(
+                  selenium.By.className("GridItem_thumb__M8icc")
+                );
 
-          const img = await div.findElement(selenium.By.css("img"));
+                const img = await div.findElement(selenium.By.css("img"));
 
-          const src = await img.getAttribute(`src`);
-          console.log(`이미지 주소 : `, src);
+                const src = await img.getAttribute(`src`);
+                console.log(`이미지 주소 : `, src);
 
-          const alt = await img.getAttribute(`alt`);
-          console.log(`이름 : `, alt);
-        }
-      });
+                const alt = await img.getAttribute(`alt`);
+                console.log(`이름 : `, alt);
+              }
+            });
+        }, 5000)
+      );
   } catch (error) {
     console.log(error);
   } finally {
+    /* 
     setTimeout(() => {
       driver.quit();
-    }, 5);
+    }, 5); */
   }
 }
 
